0d068b709cc443b0eac4afeaa57ff017609ae965
Max Garvey <maxgarvey@gmail.com>
2016-10-27 02:02:33 -0700

    add shuffle, deal, todos, rank_index.

--- deck.js
+++ deck.js
@@ -1,13 +1,70 @@
 var deck = (function(){
+    var aces_high = false;
 
     var Card = function(suit, rank, name){
         this.suit = suit;
         this.rank = rank;
         this.name = name;
+
+        switch(rank){
+            case 'ace': 
+                if (aces_high){this.rank_index = 12;}
+                else {this.rank_index = 0;}
+                break;
+            case 'two':
+                if (aces_high){this.rank_index = 0;}
+                else {this.rank_index = 1;}
+                break;
+            case 'three':
+                if (aces_high){this.rank_index = 1;}
+                else {this.rank_index = 2;}
+                break;
+            case 'four':
+                if (aces_high){this.rank_index = 2;}
+                else {this.rank_index = 3;}
+                break;
+            case 'five':
+                if (aces_high){this.rank_index = 3;}
+                else {this.rank_index = 4;}
+                break;
+            case 'six':
+                if (aces_high){this.rank_index = 4;}
+                else {this.rank_index = 5;}
+                break;
+            case 'seven':
+                if (aces_high){this.rank_index = 5;}
+                else {this.rank_index = 6;}
+                break;
+            case 'eight':
+                if (aces_high){this.rank_index = 6;}
+                else {this.rank_index = 7;}
+                break;
+            case 'nine':
+                if (aces_high){this.rank_index = 7;}
+                else {this.rank_index = 8;}
+                break;
+            case 'ten':
+                if (aces_high){this.rank_index = 8;}
+                else {this.rank_index = 9;}
+                break;
+            case 'jack':
+                if (aces_high){this.rank_index = 9;}
+                else {this.rank_index = 10;}
+                break;
+            case 'queen':
+                if (aces_high){this.rank_index = 10;}
+                else {this.rank_index = 11;}
+                break;
+            case 'king':
+                if (aces_high){this.rank_index = 11;}
+                else {this.rank_index = 12;}
+                break;
+        }
     };
 
     var deck = {
         'initialize': function(){
+            // in order cards defined; like opening a fresh deck.
             this.cards = [
                 new Card('diamonds', 'ace', 'ace of diamonds'),
                 new Card('diamonds', 'two', 'two of diamonds'),
@@ -64,33 +121,45 @@ var deck = (function(){
             ];
         },
         'shuffle': function(){
+            // only works after initialize() called
             if (this.cards !== undefined) {
+                // 2 indices to be switched
                 var cardIndex = this.cards.length;
-                var currentCard;
                 var switchIndex;
+                // buffer cardIndex card during switch
+                var currentCard;
 
                 while (0 < cardIndex) {
-                    switchIndex = Math.floor(Math.random()*cardIndex);
+                    // pick a random card to switch with cards in order
+                    switchIndex = Math.floor(
+                        Math.random()*cardIndex);
                     cardIndex = cardIndex - 1;
-
                     // save to buffer
                     currentCard = this.cards[cardIndex];
-
+                    // perform switch
                     this.cards[cardIndex] = this.cards[switchIndex];
                     this.cards[switchIndex] = currentCard;
                 }
             }
         },
         'deal': function(num_cards){
+            // initialize dealt cards array
             var dealt_cards = [];
             for (var i = 0; i<num_cards; i++) {
+                // add cards from the front of the deck
                 dealt_cards.push(
                     this.cards.shift()
                 );
             }
             return dealt_cards;
         },
+        'addCardToDeck': function(card, top_of_deck=true) {
+            // TODO: implement this
+        },
+        'cutTheDeck': function(deviation_from_middle=.2){
+            // TODO: implement this
+        }
     };
-
+    // return object
     return deck;
 }());
